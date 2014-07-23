bff3d05474c9b0a38d39e7aea16c25eee48b2abb
pascalc <pascal.chevrel@free.fr>
2014-07-23 23:33:09 +0200

    user realpath() for DATA path

--- app/inc/init.php
+++ app/inc/init.php
@@ -3,7 +3,7 @@ use Community\Directory;
 
 date_default_timezone_set('Europe/Paris');
 
-define('DATA', __DIR__ . '/../../data');
+define('DATA',       realpath(__DIR__ . '/../../data') . '/');
 define('CACHE_PATH', realpath(__DIR__ . '/../../cache/') . '/');
 
 require_once __DIR__ . '/../../vendor/autoload.php';
